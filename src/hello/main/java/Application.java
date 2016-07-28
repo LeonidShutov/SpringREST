58576e64786bdb951c4d019b886d2016401cf816
LeonidShutov <shizopotam@gmail.com>
2016-07-28 15:45:04 +0300

    main controller functionality

---
+++ src/hello/main/java/Application.java
@@ -0,0 +1,14 @@
+package hello.main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shizo on 28.07.2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/hello/main/java/Greeting.java
@@ -0,0 +1,24 @@
+package hello.main.java;
+
+/**
+ * Created by Leonid on 28.07.2016.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+
+}
